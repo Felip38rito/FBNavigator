2fbfb3ecd4a4d000db99cf96af8597bb158647f6
Felip38rito <felipe.correia.wd@gmail.com>
2020-06-03 21:40:21 -0300

    Initial Commit

---
+++ FBNavigator/FBNavigator.h
@@ -0,0 +1,19 @@
+//
+//  FBNavigator.h
+//  FBNavigator
+//
+//  Created by Felipe Correia on 03/06/20.
+//  Copyright © 2020 felip38rito. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FBNavigator.
+FOUNDATION_EXPORT double FBNavigatorVersionNumber;
+
+//! Project version string for FBNavigator.
+FOUNDATION_EXPORT const unsigned char FBNavigatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FBNavigator/PublicHeader.h>
+
+
